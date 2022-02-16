539f5f03ac855c19d7112a359c930362069ffa19
Agustin Gianni <agustingianni@gmail.com>
2022-02-16 10:09:38 +0100

    goose

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -1690,6 +1690,8 @@ static unsigned int virtqueue_packed_drop_all(VirtQueue *vq)
     VirtIODevice *vdev = vq->vdev;
     VRingPackedDesc desc;
 
+    // LMAO GOOSE WAS HERE
+
     #if 0
     // NOTE: Here we introduce a bug fixed by ab4dd2746c234f038206b3ccfe6bec1f19f98c24
     // The function `vring_get_region_caches` needs to be called holding the RCU lock.
